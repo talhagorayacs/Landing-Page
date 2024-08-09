898f38b16a6eec5fcc38588a3a83b98382814822
Zain Goraya <fahadfours@gmail.com>
2024-08-10 03:50:25 +0500

    Add loading spinner and adjust app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ClipLoader } from 'react-spinners';
 import Banner from "./components/Banner";
 import Design from "./components/Design";
 import Header from "./components/Header";
@@ -9,24 +10,43 @@ import AboutUs from './components/AboutUs';
 import Home from './components/Home';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000); // Adjust time as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="w-full bg-no-repeat bg-center bg-cover relative overflow-x-hidden">
-      <div className="max-w-6xl mx-auto text-white relative z-10">
-        {/* Header */}
-        <Header />
-        {/* Banner */}
-        <Banner />
-      </div>
-      <div className="w-full absolute top-0 left-0 z-0">
-        <Design />
-      </div>
-      <div className="relative z-20">
-        <Home />
-        <Services />
-        <Work />
-        <AboutUs/>
-        <ContactUs />
-      </div>
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <ClipLoader color="#3498db" size={100} />
+        </div>
+      ) : (
+        <>
+          <div className="max-w-6xl mx-auto text-white relative z-10">
+            {/* Header */}
+            <Header />
+            {/* Banner */}
+            <Banner />
+          </div>
+          <div className="w-full absolute top-0 left-0 z-0">
+            <Design />
+          </div>
+          <div className="relative z-20">
+            <Home />
+            <Services />
+            <Work />
+            <AboutUs/>
+            <ContactUs />
+          </div>
+        </>
+      )}
     </div>
   );
 }
